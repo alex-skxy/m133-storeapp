b80abf0a40814ff343751ab62aeb77ab53031a91
alex-skxy <aleksandra.kina@sluz.ch>
2022-01-12 10:20:21 +0100

    update shopping cart code

--- backend/shoppingCartAPI.ts
+++ backend/shoppingCartAPI.ts
@@ -1,25 +1,17 @@
 import {Router} from 'https://deno.land/x/oak@v9.0.1/mod.ts';
 
 class shoppingCartEntry {
-    productId: string;
-    amount: number;
-
-    constructor(productId: string, amount: string) {
-        this.productId = productId;
-        this.amount = amount;
+    constructor(public productId: string, public amount: number) {
     }
 }
 
 const shoppingCartRouter = new Router();
 shoppingCartRouter.put('/shoppingCart', async (context) => {
-    const newItem: shoppingCartEntry = await context.request.body({type: "json"});
-    if((await context.cookies.get('shoppingcart')) === undefined){
-        await context.cookies.set('shoppingcart', '[]');
-    }
-    let shoppingCart = JSON.parse((await context.cookies.get('shoppingcart')) as string);
-    //TODO: fix can multiple of the same product
-    await shoppingCart.push(shoppingCartEntry);
-    context.cookies.set('shoppingcart', shoppingCart.stringify());
-
+    const newItem: shoppingCartEntry = await (await context.request.body({type: "json"})).value;
+    let shoppingcart = await context.cookies.get('shoppingcart') || '[]';
+    await context.cookies.set('shoppingcart', JSON.stringify(
+        JSON.parse(shoppingcart).push(newItem)
+    ));
 });
 export default shoppingCartRouter;
+
