faea119dd33c08b91a88ee98b402a6439b2e0c23
Daniel Santos Sousa <Daniel_SantosSousa@sluz.ch>
2022-01-12 11:25:14 +0100

    add methods to shoppingCart

--- backend/shoppingCartAPI.ts
+++ backend/shoppingCartAPI.ts
@@ -8,7 +8,7 @@ class ShoppingCartEntry {
 const shoppingCartRouter = new Router();
 shoppingCartRouter.put('/shoppingCart', async (context) => {
     const newItem: ShoppingCartEntry = await (await context.request.body({type: "json"})).value;
-    let shoppingcart = await context.cookies.get('shoppingcart') || '[]';
+    const shoppingcart = await context.cookies.get('shoppingcart') || '[]';
     const shoppingCart:ShoppingCartEntry[] = JSON.parse(shoppingcart);
     const entryIndex = shoppingCart.findIndex(entry => entry.productId === newItem.productId);
     if(entryIndex != -1){
@@ -17,6 +17,18 @@ shoppingCartRouter.put('/shoppingCart', async (context) => {
         shoppingCart.push(newItem);
     }
     await context.cookies.set('shoppingcart', JSON.stringify(shoppingCart));
+}).get('/shoppingCart', async (context) => {
+    return await context.cookies.get('shoppingcart') || '[]';
+}).get('/shoppingCart/total', async (context) => {
+    const shoppingcart = await context.cookies.get('shoppingcart') || '[]';
+    const shoppingCart:ShoppingCartEntry[] = JSON.parse(shoppingcart);
+    let total:number = 0.0;
+    shoppingCart.forEach(entry => {
+        total += (await (await fetch(`/api/product/${entry.productId}`)).json()).specialOffer * entry.amount;
+    });
+    return total;
+}).post('/shoppingCart/purchase', async (context) => {
+    context.cookies.set('shoppingcart', '[]');
 });
 export default shoppingCartRouter;
 
